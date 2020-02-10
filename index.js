8d89c65a5b7445f678351739829fd7785f5626ba
Mimmi <mimmipi@users.noreply.github.com>
2020-02-10 22:57:52 +0200

    MongoDB third try

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const mongoose = require('mongoose')
 
