1737bd1e63a425ebc2a5800f46d74914a48de23e
Mimmi <mimmipi@users.noreply.github.com>
2020-02-10 20:22:35 +0200

    Part 3 herokuun

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.json(person)
   })
 
-const PORT = 3001
-app.listen(PORT, () => {
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+  })
