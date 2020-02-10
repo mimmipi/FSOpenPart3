ea1a75646296d1f12779490a1d76ac7f0110b679
Mimmi <mimmipi@users.noreply.github.com>
2020-02-10 23:04:38 +0200

    MongoDB third try

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.json(person)
   })
 
-  const PORT = process.env.PORT 
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
